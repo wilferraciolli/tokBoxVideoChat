22a1eedb54684e33a9d2758abc13daef06225649
Wiliam Ferraciolli <wiliam.ferraciolli@mhr.co.uk>
2018-04-06 11:46:23 +0100

    Initial project set up

---
+++ tokBox/src/main/java/com/wiltech/tokBox/TokBoxApplication.java
@@ -0,0 +1,12 @@
+package com.wiltech.tokBox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokBoxApplication.class, args);
+	}
+}
